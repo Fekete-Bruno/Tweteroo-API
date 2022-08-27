81ed846a1c6681f107c7b1e8ea29b109fbb3e598
Fekete-Bruno <brunohffekete@gmail.com>
2022-08-26 21:19:25 -0300

    feat: get is working

--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
 import express from 'express';
 
 const app = express();
+const  tweetsHARDCODE = [
+    {username: "patrick",tweet: "I'm Hungry"},
+    {username: "spongebob",tweet: "Let's make some krabby patties!!"},
+    {username: "spongebob",tweet: "I'm READY!!!"},
+    {username: "patrick",tweet: "I'm Hungry"},
+    {username: "spongebob",tweet: "Let's make some krabby patties!!"},
+    {username: "spongebob",tweet: "I'm READY!!!"},
+    {username: "patrick",tweet: "I'm Hungry"},
+    {username: "spongebob",tweet: "Let's make some krabby patties!!"},
+    {username: "spongebob",tweet: "I'm READY!!!"},
+    {username: "patrick",tweet: "I'm Hungry"},
+    {username: "spongebob",tweet: "Let's make some krabby patties!!"},
+    {username: "spongebob",tweet: "I'm READY!!!"},
+];
+const usersHARDCODE= [
+    {username:"patrick",avatar:"http://logos-download.com/wp-content/uploads/2016/09/Patrick_Star_picture_logo.png"},
+    {username: 'spongebob', avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" }
+];
 
 app.get('/tweets',(req,res)=>{
-    console.log('get');
-    res.send('OK')
+    const recentTweets = tweetsHARDCODE.filter((e,index)=>{return(index<10)});
+    recentTweets.map((element)=>element.avatar=usersHARDCODE.find(el=>el.username===element.username).avatar);
+    res.send(recentTweets);
 });
 
 app.listen(5000,()=>console.log('Listening on port 5000'));
