9af2e041df8336c86924b9e3e70534344a8dd161
Fekete-Bruno <brunohffekete@gmail.com>
2022-08-28 10:30:36 -0300

    feat: post sign-up is working

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+
 const  tweetsHARDCODE = [
     {username: "patrick",tweet: "I'm Hungry"},
     {username: "spongebob",tweet: "Let's make some krabby patties!!"},
@@ -24,6 +26,17 @@ app.get('/tweets',(req,res)=>{
     const recentTweets = tweetsHARDCODE.filter((e,index)=>{return(index<10)});
     recentTweets.map((element)=>element.avatar=usersHARDCODE.find(el=>el.username===element.username).avatar);
     res.send(recentTweets);
+});
+
+app.post('/sign-up',(req,res)=>{
+    const { username, avatar } =  req.body;
+    if(!username || !avatar){
+        return res.status(400).send("Todos os campos são obrigatórios!");
+    }
+
+    usersHARDCODE.push({username,avatar});
+    console.log(usersHARDCODE);
+    res.status(201).send("OK");
 });
 
 app.listen(5000,()=>console.log('Listening on port 5000'));
